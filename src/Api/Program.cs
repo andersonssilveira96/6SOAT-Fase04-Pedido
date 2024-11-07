6ad24e0d2d6783152ef834e4ab3add99da9ce6a8
Anderson Souza <andersonssilveira96@gmail.com>
2024-11-06 22:52:24 -0300

    Limpando projeto

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TechChallenge API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TechChallenge Pedido API", Version = "v1" });
 
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
     {
